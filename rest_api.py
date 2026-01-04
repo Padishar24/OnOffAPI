e0e578fe40957d2576af47883c75edaa3c23398b
Padishar24 <burkhardzeiner+git@googlemail.com>
2026-01-04 22:56:10 +0100

    Switch monitor control from xrandr to wlr-randr for Wayland

--- rest_api.py
+++ rest_api.py
@@ -4,21 +4,25 @@ import os
 
 from modules.fancontroller import setFanSpeed, getCpuTemperature, getFanSpeed
 
-def turn_off_monitor(display=":0.0", output="HDMI-1"):
+def turn_off_monitor(display=":0.0", output="HDMI-A-1"):
     try:
         #command = f"DISPLAY={display} xrandr --output {output} --off"
-        command = f"xrandr -display {display} --output {output} --off"
-        print (command)
+        #command = f"xrandr -display {display} --output {output} --off"
+        # wayland
+        command = f"wlr-randr --output {output} --off"
+        #print (command)
         subprocess.call(command, shell=True)
         return f"Monitor {output} turned off successfully on display {display}."
     except Exception as e:
         return f"An error occurred: {e}"
 
-def turn_on_monitor(display=":0.0", output="HDMI-1", mode="1280x800 --rate 30"):
+def turn_on_monitor(display=":0.0", output="HDMI-A-1", mode="1280x800 --rate 30"):
     try:
         #command = f"DISPLAY={display} xrandr --output {output} --mode {mode}"
-        command = f"xrandr -display {display} --output {output} --mode {mode}"
-        print (command)
+        #command = f"xrandr -display {display} --output {output} --mode {mode}" # for X
+        # wayland
+        command = f"wlr-randr --output {output} --on"
+        #print (command)
         subprocess.call(command, shell=True)
         return f"Monitor {output} turned on successfully on display {display}."
     except Exception as e:
@@ -26,11 +30,11 @@ def turn_on_monitor(display=":0.0", output="HDMI-1", mode="1280x800 --rate 30"):
 
 def create_app():
     print("Starting Snips Services")
-            
+
     ################################################################
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "snipsservices-boom-!!123"
-    
+
     # add a route to turn the display on or off
     @app.route('/display/<state>')
     def display(state):
@@ -39,10 +43,10 @@ def create_app():
             res = turn_on_monitor()
         elif state == "off":
             res = turn_off_monitor()
-        return res            
-    
+        return res
+
     @app.route('/ping/<ip>')
-    def ping(ip):     
+    def ping(ip):
         response = os.system("ping -c 1 " + ip + " > /dev/null 2>&1")
         if response == 0:
             return {'ResponseCode': 'ONLINE'}
@@ -52,12 +56,12 @@ def create_app():
     # @app.route('/', methods=['GET', 'POST'])
     # def index():
     #     return render_template('index.html')
-    
-    @app.route('/fan/<speed>') 
+
+    @app.route('/fan/<speed>')
     def fan(speed):
         setFanSpeed(int(speed))
         return {"fan_speed": speed}
-    
+
     @app.route('/temp')
     def temp():
         # global cur_temp, fan_speed
@@ -76,9 +80,9 @@ def create_app():
         #     setFanSpeed(25)
         # elif temp < 35. and fan_speed == 25:
         #     setFanSpeed(0)
-            
+
         # print (f"Temperature: {temp}°C, Fan Speed: {fan_speed}%")
-            
+
         # return the CPU temperature
         return {"temperature": getCpuTemperature(), "fan_speed": getFanSpeed()}
 
