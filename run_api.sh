8b9fb1b1f9a11b1525e00fdfa0274eb1036a2155
Padishar24 <burkhardzeiner+git@googlemail.com>
2026-01-04 22:40:07 +0100

    Refactor run_api.sh to use virtual environment and add .gitignore for .venv

--- run_api.sh
+++ run_api.sh
@@ -2,5 +2,6 @@
 set -e
 export DISPLAY=:0.0
 export XAUTHORITY=/home/pi/.Xauthority
-source "/opt/python3-flask-env/bin/activate"
+#source "/opt/python3-flask-env/bin/activate"
+source /home/pi/OnOffAPI/.venv/bin/activate
 python3 -u /home/pi/OnOffAPI/rest_api.py
