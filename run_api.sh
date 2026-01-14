461803622a83d9619121c27c6850717f76567cc6
Padishar24 <burkhardzeiner+git@googlemail.com>
2026-01-14 20:17:23 +0100

    Adaptions for wayland

--- run_api.sh
+++ run_api.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -e
-export DISPLAY=:0.0
-export XAUTHORITY=/home/pi/.Xauthority
+export WAYLAND_DISPLAY=wayland-0
+export XDG_RUNTIME_DIR=/run/user/1000
+#mkdir -p "$XDG_RUNTIME_DIR"
+#chmod 700 "$XDG_RUNTIME_DIR"
+
 #source "/opt/python3-flask-env/bin/activate"
 source /home/pi/OnOffAPI/.venv/bin/activate
 python3 -u /home/pi/OnOffAPI/rest_api.py
